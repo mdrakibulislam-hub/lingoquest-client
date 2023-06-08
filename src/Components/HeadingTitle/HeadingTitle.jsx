b7822b7b3ec9a6d63a0846196d68af9f3dff26c6
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-06-09 01:28:49 +0600

    instructor profile classes fetched by axios and showed in card

--- src/Components/HeadingTitle/HeadingTitle.jsx
+++ src/Components/HeadingTitle/HeadingTitle.jsx
@@ -4,7 +4,7 @@ const HeadingTitle = ({ children }) => {
     return (
         <div className="flex items-center my-8">
             <div className="h-px bg-secondary w-full"></div>
-            <h2 className="text-secondary text-3xl font-bold font-playfair text-center w-[50rem]">{children}</h2>
+            <h2 className="text-secondary text-3xl font-bold font-playfair text-center w-full">{children}</h2>
             <div className="h-px bg-secondary w-full"></div>
         </div>
     );
--- src/Pages/InstructorProfile/InstructorProfile.jsx
+++ src/Pages/InstructorProfile/InstructorProfile.jsx
@@ -1,12 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import HeadingTitle from "../../Components/HeadingTitle/HeadingTitle";
+import ClassCard from "../../Components/ClassCard/ClassCard";
 
 const InstructorProfile = () => {
 
     const { id } = useParams();
+    const [instructor, setInstructor] = useState([]);
+
+    useEffect(() => {
+        axios(`http://localhost:5000/instructor/${id}`).then(data => setInstructor(data.data))
+    }, [id]);
+
+    const { _id, image, title, name, email, totalStudents } = instructor;
+
+    const [classes, setClasses] = useState([]);
+    useEffect(() => { axios(`http://localhost:5000/allclasses/${email}`).then(data => setClasses(data.data)) }, [email]);
+
+    console.log(classes);
 
     return (
-        <div>
-            <h1>{id}</h1>
+        <div className="main-container">
+
+            <div className="my-16 flex gap-6 items-center bg-primary rounded-lg">
+                <figure><img className="rounded-lg" src={image} alt="" /></figure>
+                <div>
+                    <h1 className="font-bold text-4xl font-playfair text-neutral">{name}</h1>
+                    <p>Email: {email}</p>
+                    <p>Total Students: {totalStudents}</p>
+                </div>
+            </div>
+
+            <HeadingTitle>Classes by {name}</HeadingTitle>
+
+            <div className="grid grid-cols-4 my-4 gap-6">
+                {classes.map(data => <ClassCard key={data._id} data={data}></ClassCard>)}
+            </div>
+
         </div>
     );
 };
