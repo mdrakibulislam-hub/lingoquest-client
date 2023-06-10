58935900997104b7611eb4e4e50c826cd6125850
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-06-10 18:03:35 +0600

    dashboard page created

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../Pages/Shared/Navbar/Navbar';
 import Footer from '../../Pages/Shared/Footer';
+import { FaBook, FaBookmark, FaBookReader, FaUserCog, FaEdit } from 'react-icons/fa';
+import { MdOutlineAddCircle } from 'react-icons/md';
 
 const Dashboard = () => {
     return (
@@ -19,18 +21,19 @@ const Dashboard = () => {
                     <div className="bg-primary drawer-side rounded-xl z-20">
                         <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                         <ul className="menu p-4 w-80 h-full bg-primary text-base-content">
+                            <h1 className='text-3xl font-playfair font-semibold mt-3 mb-6 mx-2'>Welcome to Dashboard</h1>
                             {/* Sidebar content here */}
                             {/* regurler user options */}
-                            <li><Link>Selected courses</Link></li>
-                            <li><Link>Enorlled courses</Link></li>
+                            <li><Link><FaBook></FaBook> Selected courses</Link></li>
+                            <li><Link><FaBookReader></FaBookReader> Enorlled courses</Link></li>
                             {/* Instructor options */}
                             <div className="divider"></div>
-                            <li><Link>Add a class</Link></li>
-                            <li><Link>My classes</Link></li>
+                            <li><Link><MdOutlineAddCircle></MdOutlineAddCircle> Add a class</Link></li>
+                            <li><Link><FaBookmark></FaBookmark> My classes</Link></li>
                             {/* Instructor options */}
                             <div className="divider"></div>
-                            <li><Link>Manage classes</Link></li>
-                            <li><Link>Manage users</Link></li>
+                            <li><Link><FaEdit></FaEdit>Manage classes</Link></li>
+                            <li><Link><FaUserCog></FaUserCog> Manage users</Link></li>
                         </ul>
 
                     </div>
