9b87b9d5c16a66df3601c02566d2e243be1ef0e0
mdrakibulislam <rakibulislamborkan@gmail.com>
2023-06-12 10:06:30 +0600

    admin feedback to class functionality added

--- src/Components/FeedBackModal/FeedBackModal.jsx
+++ src/Components/FeedBackModal/FeedBackModal.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import { data } from 'autoprefixer';
+import axios from 'axios';
+import React, { useRef, useState } from 'react';
 
 const FeedBackModal = ({ id }) => {
+    const feedback = useRef('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const userFeedback = feedback.current.value
+        console.log(userFeedback);
+        axios.patch(`http://localhost:5000/sendfeedback/class/${id}`, { userFeedback }).then(data => { console.log(data.data); })
+    }
+
+
     return (
         <>
             {/* modal body */}
             <input type="checkbox" id={id} className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box">
-                    <h3 className="text-lg font-bold">Give feedback to instructor</h3>
-                    <p className="py-4">{id}</p>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
-                    <button className="btn">Send</button>
+                    <h3 className="text-2xl font-bold mb-4 font-playfair">Give feedback to instructor</h3>
+                    <form onSubmit={handleSubmit} className='flex flex-col gap-4' action="">
+                        <textarea ref={feedback} className='rounded-md p-3 bg-gray-100 text-base outline-none focus:shadow-inner' name="" id="" cols="20" rows="10" placeholder='Please write down your feedback here...'></textarea>
+                        <button type='submit' className="btn btn-secondary normal-case">Send</button>
+                    </form>
                 </div>
                 <label className="modal-backdrop" htmlFor={id}>Close</label>
             </div>
